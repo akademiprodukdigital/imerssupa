ff2df70110d13d10468baba3e1335548f22f7591
akademiprodukdigital <produkdigitalakademi@gmail.com>
2026-09-16 15:10:25 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'iMersSUPA',
+  description: 'Membership & Digital Content Platform',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
